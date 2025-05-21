cea11d61ff8512b8dc380435374db4732104078e
flavienrak <flavien.andrisoarak@gmail.com>
2025-05-21 19:37:15 +0300

    fix(previsualisation): update previsualisation page

--- src/components/client-report/Previsualisation.tsx
+++ src/components/client-report/Previsualisation.tsx
@@ -430,7 +430,7 @@ export default function Previsualisation({
           </div>
         </div>
 
-        {editCvAnonym.label && (
+        {showHeader && editCvAnonym.label && (
           <div className="fixed top-0 left-0 h-full w-full flex justify-center items-center p-4 rounded-xl bg-black/70 backdrop-blur-sm">
             <div className="h-full w-full flex justify-center items-center">
               <Card className="flex flex-col bg-gradient-to-br from-[#1F2437] via-[#161A2A] to-[#0A0E17] backdrop-blur-lg border-[#FF6B00]/10 shadow-[0_0_30px_rgba(255,107,0,0.1)]">
@@ -571,96 +571,97 @@ export default function Previsualisation({
           </div>
         )}
 
-        {typeof editSyntheseAnonym.questionNumber === 'number' && (
-          <div className="fixed top-0 left-0 h-full w-full flex justify-center items-center p-4 rounded-xl bg-black/70 backdrop-blur-sm">
-            <div className="h-full w-full flex justify-center items-center">
-              <Card className="flex flex-col bg-gradient-to-br from-[#1F2437] via-[#161A2A] to-[#0A0E17] backdrop-blur-lg border-[#FF6B00]/10 shadow-[0_0_30px_rgba(255,107,0,0.1)]">
-                <div className="flex flex-col gap-4">
-                  <div className="flex justify-between items-center">
-                    <h2 className="text-2xl font-medium text-white leading-relaxed">
-                      {
-                        syntheseLabels.find(
-                          (item) =>
-                            item.questionNumber ===
-                            editSyntheseAnonym.questionNumber
-                        )?.value
-                      }
-                    </h2>
-                    <i
-                      onClick={() =>
-                        setEditSyntheseAnonym(initialEditSyntheseAnonym)
-                      }
-                      className="h-8 w-8 flex justify-center items-center rounded-full hover:bg-white/10 cursor-pointer"
-                    >
-                      <X />
-                    </i>
-                  </div>
-
-                  <form
-                    onSubmit={handleEditSyntheseAnonym}
-                    className="flex-1 flex flex-col gap-4"
-                  >
-                    <div className="flex flex-col gap-2">
-                      <textarea
-                        value={editSyntheseAnonym.value}
-                        onChange={(event) =>
-                          setEditSyntheseAnonym((prev) => ({
-                            ...prev,
-                            value: event.target.value,
-                          }))
+        {showHeader &&
+          typeof editSyntheseAnonym.questionNumber === 'number' && (
+            <div className="fixed top-0 left-0 h-full w-full flex justify-center items-center p-4 rounded-xl bg-black/70 backdrop-blur-sm">
+              <div className="h-full w-full flex justify-center items-center">
+                <Card className="flex flex-col bg-gradient-to-br from-[#1F2437] via-[#161A2A] to-[#0A0E17] backdrop-blur-lg border-[#FF6B00]/10 shadow-[0_0_30px_rgba(255,107,0,0.1)]">
+                  <div className="flex flex-col gap-4">
+                    <div className="flex justify-between items-center">
+                      <h2 className="text-2xl font-medium text-white leading-relaxed">
+                        {
+                          syntheseLabels.find(
+                            (item) =>
+                              item.questionNumber ===
+                              editSyntheseAnonym.questionNumber
+                          )?.value
                         }
-                        className={`flex-1 min-h-[14rem] min-w-[32rem] bg-[#0A0E17]/80 border border-gray-800 rounded-lg px-4 py-3 text-white placeholder-gray-400 outline-none focus:outline-none focus:ring-2 focus:ring-[#FF6B00]/30 focus:border-[#FF6B00]/20 resize-none ${
-                          editSyntheseLoading
-                            ? 'opacity-50'
-                            : 'hover:bg-[#0A0E17]/90'
-                        }`}
-                        placeholder={editSyntheseAnonym.initialValue}
-                      />
+                      </h2>
+                      <i
+                        onClick={() =>
+                          setEditSyntheseAnonym(initialEditSyntheseAnonym)
+                        }
+                        className="h-8 w-8 flex justify-center items-center rounded-full hover:bg-white/10 cursor-pointer"
+                      >
+                        <X />
+                      </i>
                     </div>
 
-                    <button
-                      type="submit"
-                      disabled={
-                        editSyntheseLoading ||
-                        editSyntheseAnonym.value.trim().length === 0 ||
-                        editSyntheseAnonym.value ===
-                          editSyntheseAnonym.initialValue
-                      }
-                      className={classNames(
-                        'w-full rounded-lg flex items-center justify-center gap-2 py-3 text-lg font-medium bg-gradient-to-r from-[#FF6B00] to-[#FF8124] text-white hover:from-[#FF8124] hover:to-[#FF9346] shadow-lg shadow-[#FF6B00]/20',
-                        editSyntheseAnonym.value ===
-                          editSyntheseAnonym.initialValue &&
-                          'opacity-50 cursor-not-allowed',
-                        editSyntheseLoading && 'opacity-50'
-                      )}
+                    <form
+                      onSubmit={handleEditSyntheseAnonym}
+                      className="flex-1 flex flex-col gap-4"
                     >
-                      {editSyntheseLoading && (
-                        <svg
-                          aria-hidden="true"
-                          role="status"
-                          className="inline w-6 h-6 animate-spin"
-                          viewBox="0 0 100 101"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                            fill="#E5E7EB"
-                          />
-                          <path
-                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                            fill="currentColor"
-                          />
-                        </svg>
-                      )}
-                      <span>Mettre à jour</span>
-                    </button>
-                  </form>
-                </div>
-              </Card>
+                      <div className="flex flex-col gap-2">
+                        <textarea
+                          value={editSyntheseAnonym.value}
+                          onChange={(event) =>
+                            setEditSyntheseAnonym((prev) => ({
+                              ...prev,
+                              value: event.target.value,
+                            }))
+                          }
+                          className={`flex-1 min-h-[14rem] min-w-[32rem] bg-[#0A0E17]/80 border border-gray-800 rounded-lg px-4 py-3 text-white placeholder-gray-400 outline-none focus:outline-none focus:ring-2 focus:ring-[#FF6B00]/30 focus:border-[#FF6B00]/20 resize-none ${
+                            editSyntheseLoading
+                              ? 'opacity-50'
+                              : 'hover:bg-[#0A0E17]/90'
+                          }`}
+                          placeholder={editSyntheseAnonym.initialValue}
+                        />
+                      </div>
+
+                      <button
+                        type="submit"
+                        disabled={
+                          editSyntheseLoading ||
+                          editSyntheseAnonym.value.trim().length === 0 ||
+                          editSyntheseAnonym.value ===
+                            editSyntheseAnonym.initialValue
+                        }
+                        className={classNames(
+                          'w-full rounded-lg flex items-center justify-center gap-2 py-3 text-lg font-medium bg-gradient-to-r from-[#FF6B00] to-[#FF8124] text-white hover:from-[#FF8124] hover:to-[#FF9346] shadow-lg shadow-[#FF6B00]/20',
+                          editSyntheseAnonym.value ===
+                            editSyntheseAnonym.initialValue &&
+                            'opacity-50 cursor-not-allowed',
+                          editSyntheseLoading && 'opacity-50'
+                        )}
+                      >
+                        {editSyntheseLoading && (
+                          <svg
+                            aria-hidden="true"
+                            role="status"
+                            className="inline w-6 h-6 animate-spin"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                              fill="#E5E7EB"
+                            />
+                            <path
+                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        )}
+                        <span>Mettre à jour</span>
+                      </button>
+                    </form>
+                  </div>
+                </Card>
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
         {!showHeader && (
           <footer className="py-4 px-6 text-center text-gray-400 text-sm">
